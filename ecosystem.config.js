e61a3540ef55dfa1ded877cca467e5e9e690ba95
benja0952346729-hash <benja0952346729@gmail.com>
2026-05-22 10:32:03 +0300

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   apps: [
-    { name: "server", script: "server.js", env: { PORT: 8000 } },
-    { name: "bot-js", script: "Bot.js",    env: { PORT: 3002 } },
-    { name: "tts",    script: "tts.js",    env: { PORT: 3001 } },
-    { name: "bot-py", script: "Bot.py",    interpreter: "python3", env: { PORT: 10000 } }
+    { name: "server", script: "server.js", env_production: { PORT: 8000 } },
+    { name: "bot-js", script: "Bot.js",    env_production: { PORT: 3002 } },
+    { name: "tts",    script: "tts.js",    env_production: { PORT: 3001 } },
+    { name: "bot-py", script: "Bot.py",    interpreter: "python3", env_production: { PORT: 10000 } }
   ]
 }
